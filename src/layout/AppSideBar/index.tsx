9537ddfd2076137988a6ecd7fda526ee0bab7885
maybis <615763004@qq.com>
2021-07-19 23:21:54 +0800

    homepage and create page mobile ui adaption

--- src/layout/AppSideBar/index.tsx
+++ src/layout/AppSideBar/index.tsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react'
-import { Button, Layout, Menu } from 'antd'
+import React from 'react'
+import { Layout, Menu } from 'antd'
 import styled from 'styled-components'
 import { Link, useLocation } from 'react-router-dom'
 import routes, { Route } from '../../routes'
 import twitterIcon from '../../assets/images/slidebarLink/twitter.svg'
 import telegramIcon from '../../assets/images/slidebarLink/telegram.svg'
 
-const { Header, Content, Footer, Sider } = Layout
-const { SubMenu } = Menu
-
 const Container = styled.div`
   height: 100vh;
   background-color: #101A2D;
   position: relative;
+  z-index: 11;
 
-  .ant-menu-root.ant-menu-vertical, .ant-menu-root.ant-menu-vertical-left, .ant-menu-root.ant-menu-vertical-right, .ant-menu-root.ant-menu-inline {
-    background: #101A2D;
-  !important;
+  .ant-menu-root.ant-menu-vertical,
+  .ant-menu-root.ant-menu-vertical-left,
+  .ant-menu-root.ant-menu-vertical-right,
+  .ant-menu-root.ant-menu-inline {
+    background: #101A2D !important;
     box-shadow: none;
   }
 `
 
+
 const CustomizedMenu = styled(Menu)`
-
-
   .ant-menu-item {
     display: flex;
     align-items: center;
@@ -77,7 +76,6 @@ const CustomizedLink = styled.div`
   }
 `
 
-
 const AppSideBar: React.FC = () => {
   const { pathname } = useLocation()
 
@@ -85,26 +83,12 @@ const AppSideBar: React.FC = () => {
     return routes.filter(route => route.path === pathname || route.match?.test(pathname))[0].path
   })()
 
-  /*const [collapsed, setCollapsed] = useState(true)
-
-  const state = {
-    collapsed: false
-  }
-
-  const onCollapse = (collapsed: boolean) => {
-    console.log(collapsed)
-    setCollapsed(collapsed)
-  }*/
-
   return (
     <Container>
-      {/*<Layout style={{ minHeight: '100vh' }}>*/}
-      {/*  <Sider collapsed={collapsed} onCollapse={onCollapse}>*/}
       <CustomizedMenu
         selectedKeys={[selectedKey]}
         mode="inline"
         theme="dark"
-        // inlineCollapsed={state.collapsed}
       >
         {
           routes.filter(route => !route.hidden).map((route: Route) => {
@@ -120,28 +104,6 @@ const AppSideBar: React.FC = () => {
           })
         }
       </CustomizedMenu>
-      {/*  </Sider>*/}
-      {/*</Layout>*/}
-      {/*<CustomizedMenu
-        selectedKeys={[selectedKey]}
-        mode="inline"
-        theme="dark"
-        inlineCollapsed={state.collapsed}
-      >
-        {
-          routes.filter(route => !route.hidden).map((route: Route) => {
-            const fillColor = (route.path === pathname || route.match?.test(pathname)) ? 'white' : '#fff'
-
-            return (
-              <Menu.Item key={route.path} icon={<route.icon fill={fillColor}  />}>
-                <Link to={route.path} style={{ userSelect: 'none', color: 'rgb(178,178,178)' }}>
-                  {route.title}
-                </Link>
-              </Menu.Item>
-            )
-          })
-        }
-      </CustomizedMenu>*/}
       <CustomizedLink>
         <a href={'https://twitter.com/banksy_finance'} rel="noreferrer" target="_blank"><img src={twitterIcon} /></a>
         <a href={'https://t.me/Banskyfinance'} rel="noreferrer" target="_blank"><img src={telegramIcon} /></a>
